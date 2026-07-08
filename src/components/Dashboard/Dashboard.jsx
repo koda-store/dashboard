97b6cb83e781b498903791a78599f31e0395b602
Rawda Saleh <rawda.saleh202@gmail.com>
2026-07-08 18:10:33 +0300

    collecting

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -8,7 +8,7 @@ import Overview from "./Overview";
 
 function Dashboard() {
   return (
-    <div className=" fade-up p-6  w-full ">
+    <div className=" fade-up   w-full ">
    <Overview/>
       <StatsCards />
      <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 ">
