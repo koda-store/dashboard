72cd8fcaef4c543a28f94b260caffc8d296f4e62
اسمك على GitHub <khaledmanaaz.123456@gmail.com>
2026-07-11 02:14:04 +0300

    Add users page

--- src/api/axios.js
+++ src/api/axios.js
@@ -25,3 +25,42 @@ api.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+
+
+
+
+
+
+/*
+
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://e-commerce-api-3wara.vercel.app",
+   withCredentials: true, 
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("dashboard-token");
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("dashboard-token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+*/
+
