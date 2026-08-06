20bebe2742cc97825d9f09c0a70613ec9435fb6b
Hayam106 <hayamabdo10696@gmail.com>
2026-08-06 03:24:25 +0300

    update dashboard

--- src/components/Dashboard/OrderStatus.jsx
+++ src/components/Dashboard/OrderStatus.jsx
@@ -32,50 +32,50 @@ function OrderStatus() {
   };
   /////////////
   const statusCards = [
-    {
-      title: "Pending",
-      count: breakdown.Pending,
-      border: "border-yellow-300 dark:border-yellow-900",
-      bg: "bg-yellow-50 dark:bg-yellow-800/20",
-      text: "text-yellow-700 dark:text-yellow-400",
-    },
-    {
-      title: "Processing",
-      count: breakdown.Processing,
-      border: "border-blue-300 dark:border-blue-900",
-      bg: "bg-blue-50 dark:bg-blue-800/20",
-      text: "text-blue-700 dark:text-blue-400",
-    },
-    {
-      title: "Confirmed",
-      count: breakdown.Confirmed,
-      border: "border-green-300 dark:border-green-900",
-      bg: "bg-green-50 dark:bg-green-800/20",
-      text: "text-green-700 dark:text-green-400",
-    },
-    {
-      title: "Shipped",
-      count: breakdown.Shipped,
-      border: "border-purple-300 dark:border-purple-900",
-      bg: "bg-purple-50 dark:bg-purple-800/20",
-      text: "text-purple-700 dark:text-purple-400",
-    },
-    {
-      title: "Delivered",
-      count: 120,
-      bg: "bg-green-50 dark:bg-green-900/20",
-      border: "border-green-200 dark:border-green-800",
-      text: "text-green-600 dark:text-green-400",
-    },
-    {
-      title: "Cancelled",
-      count: breakdown.Cancelled,
-      border: "border-red-300 dark:border-red-800",
-      bg: "bg-red-50 dark:bg-red-900/20",
-      text: "text-red-700 dark:text-red-500",
-    },
-  ];
-  if (error) {
+     {
+    title: "Pending",
+    count: breakdown.Pending,
+    border: "border-yellow-300 dark:border-yellow-900",
+    bg: "bg-yellow-50 dark:bg-yellow-800/20",
+    text: "text-yellow-700 dark:text-yellow-400",
+  },
+  {
+    title: "Processing",
+    count: breakdown.Processing,
+    border: "border-blue-300 dark:border-blue-900",
+    bg: "bg-blue-50 dark:bg-blue-800/20",
+    text: "text-blue-700 dark:text-blue-400",
+  },
+  {
+    title: "Confirmed",
+    count: breakdown.Confirmed,
+    border: "border-green-300 dark:border-green-900",
+    bg: "bg-green-50 dark:bg-green-800/20",
+    text: "text-green-700 dark:text-green-400",
+  },
+  {
+    title: "Shipped",
+    count: breakdown.Shipped,
+    border: "border-purple-300 dark:border-purple-900",
+    bg: "bg-purple-50 dark:bg-purple-800/20",
+    text: "text-purple-700 dark:text-purple-400",
+  },
+  {
+    title: "Delivered",
+    count: breakdown.Delivered,
+    border: "border-emerald-300 dark:border-emerald-900 ",
+    bg: "bg-emerald-50 dark:bg-emerald-800/20",
+    text: "text-emerald-700 dark:text-emerald-400",
+  },
+  {
+    title: "Cancelled",
+    count: breakdown.Cancelled,
+    border: "border-red-300 dark:border-red-900",
+    bg: "bg-red-50 dark:bg-red-800/20",
+    text: "text-red-700 dark:text-red-400",
+  },
+];
+if (error) {
     return (
       <div className="bg-white  dark:bg-slate-900 mt-5 dark:border-red-500/20 rounded-2xl shadow-xl border border-red-200 p-6">
         <p className="text-center text-red-500">{error}</p>
@@ -93,8 +93,6 @@ function OrderStatus() {
       <h2 className="text-xl mb-5 font-semibold  text-gray-600 dark:text-gray-400">
         Live Fulfillment Breakdown
       </h2>
-
-
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {statusCards.map((card, index) => (
           <div
@@ -104,9 +102,8 @@ function OrderStatus() {
             <h3 className={`${card.text} text-lg font-semibold`}>
               {card.title}
             </h3>
-
-            <p className={`${card.text} text-xl font-bold mt-3`}>
-              {card.count}
+ <p className={`${card.text} text-xl font-bold mt-3`}>
+ {card.count}
             </p>
           </div>
         ))}
